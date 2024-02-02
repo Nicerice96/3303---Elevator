f542deba9634a78e7208052eea521b99eecf5a49
Mahad T <mahad.toukaleh56@gmail.com>
2024-02-02 17:33:11 -0500

    - Made Junit test for Scheduler

---
+++ src/FloorSubsystemTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+public class FloorSubsystemTest {
+
+
+}
