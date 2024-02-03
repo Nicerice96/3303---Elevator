5ad6789a6b8fac5bb2bbf5dcc913d46456ecd8e1
Nabeel Azard <117410124+na2099@users.noreply.github.com>
2024-02-03 16:22:13 -0500

    Add files via upload
    Added JavaDoc comments to all the classes.

---
+++ src/src/SchedulerSystemTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
+public class SchedulerSystemTest {
+
+    private SchedulerSystem schedulerSystem;
+
+    @Before
+    public void setUp() throws Exception {
+        schedulerSystem = new SchedulerSystem();
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testDataTransfer() throws InterruptedException {
+        ArrayList<Object> testData = new ArrayList<>(Arrays.asList("0", "1", "UP", "2"));
+
+        SchedulerSystem.putData(testData);
+
+        Thread.sleep(500);
+
+        ArrayList<Object> receivedData = SchedulerSystem.getData();
+
+        assertEquals("The received data should match the test data sent.", testData, receivedData);
+    }
+
+
+}
