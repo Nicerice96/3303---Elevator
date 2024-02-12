30d8fd08d62cbcdac9e93d14381839fcdd26528f
HamzaAlsarakbi <hamoz.sq78@gmail.com>
2024-02-12 12:35:21 -0500

    added state files and changed def files

--- src/src/defs/ElevatorDefs.java
+++ src/src/defs/ElevatorDefs.java
@@ -1,12 +1,12 @@
 package src.defs;
 
 public class ElevatorDefs {
-    public final long ADJACENT_FLOOR_TIME = 8628; // ms
-    public final float DOOR_OPENING_TIME = 3364.28571F; // ms
-    public final float DOOR_CLOSING_TIME = 3324.28571F; // ms
-    public final float LOADING_TIME = 4714.28571F; // ms
-    public final float UNLOADING_TIME = 4712.85714F; // ms
-    public final float ACCELERATION = 1.0F; // m/s^2
-    public final float DECELERATION = 1.0F; // m/s^2
-    public final float MAX_SPEED = 5.0F; // m/s
+    public static final long ADJACENT_FLOOR_TIME = 8628; // ms
+    public static final long DOOR_OPENING_TIME = 3364; // ms
+    public static final long DOOR_CLOSING_TIME = 3324; // ms
+    public static final long LOADING_TIME = 4714; // ms
+    public static final long UNLOADING_TIME = 4713; // ms
+    public static final float ACCELERATION = 1.0F; // m/s^2
+    public static final float DECELERATION = 1.0F; // m/s^2
+    public static final float MAX_SPEED = 5.0F; // m/s
 }
