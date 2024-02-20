2d9dffc9a753aa24013a03bfbba9953484e391c8
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2024-02-19 21:19:18 -0500

    Implemented scheduler states

--- src/src/scheduler_state/SchedulerIdleState.java
+++ src/src/scheduler_state/SchedulerIdleState.java
@@ -1,8 +1,17 @@
 package src.scheduler_state;
 
+import src.SchedulerSystem;
+
 public class SchedulerIdleState extends SchedulerState  {
     @Override
     public void handle() {
-        // TODO: handler()
+
+        while (true){
+
+            if (SchedulerSystem.receievedData()){
+                SchedulerSystem.setState(new SchedulerProcessingRequestState());
+            }
+        }
+
     }
 }
--- src/src/scheduler_state/SchedulerProcessingRequestState.java
+++ src/src/scheduler_state/SchedulerProcessingRequestState.java
@@ -1,8 +1,17 @@
 package src.scheduler_state;
 
+import src.SchedulerSystem;
+import src.elevator.elevator_state.ElevatorMovingState;
+
 public class SchedulerProcessingRequestState extends SchedulerState  {
     @Override
     public void handle() {
-        // TODO: handler()
+
+        SchedulerSystem.pollElevators();
+
+        if (!SchedulerSystem.receievedData()) {
+            SchedulerSystem.setState(new SchedulerIdleState());
+        }
+
     }
 }
